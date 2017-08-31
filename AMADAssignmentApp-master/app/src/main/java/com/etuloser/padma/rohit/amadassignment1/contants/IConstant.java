57ed4622c7e4345738a44a5ea880b0de8c38509e
dnayak1 <dnayak1@uncc.edu>
2017-08-31 18:25:43 -0400

    all project added

--- AMADAssignmentApp-master/app/src/main/java/com/etuloser/padma/rohit/amadassignment1/contants/IConstant.java
+++
@@ -1,9 +0,0 @@
-package com.etuloser.padma.rohit.amadassignment1.contants;
-
-/**
- * Created by Rohit on 8/30/2017.
- */
-
-public class IConstant {
-    public static String url="http://13.59.255.195:5001/";
-}
